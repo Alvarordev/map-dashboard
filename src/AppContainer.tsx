c41df95ad73064bfa3766f404e57bf7f6cd70ab1
alvaro <alvarord519@gmail.com>
2024-01-13 19:47:25 -0500

    changed server status

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 import { store } from "./redux/store";
-import FallbackLoader from "./components/loaders/FallbackLoader";
-import axios from "axios";
 
 import App from "./App";
 import { ModalProvider } from "./context/ModalProvider";
 
-const ErrorComponent = ({ errorMessage }: { errorMessage: string | null }) => (
-  <div className="text-red-500 font-bold text-center">{errorMessage}</div>
-);
-
 const AppContainer = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const checkServerStatus = async () => {
-      try {
-        await axios.get("/server-status");
-      } catch (err) {
-        setError("Server is down. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkServerStatus();
-  }, []);
-
-  if (loading || error) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        {loading ? <FallbackLoader /> : <ErrorComponent errorMessage={error} />}
-      </div>
-    );
-  }
-
   return (
     <Provider store={store}>
       <ModalProvider>
