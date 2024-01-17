853092d50a98e139eb384f5db57f4b2561d98157
alvaro <alvarord519@gmail.com>
2024-01-16 20:08:47 -0500

    Adding lazy loading

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,11 +1,14 @@
+import { lazy } from "react";
+
 import Dashboard from "../pages/Dashboard";
 import Empresas from "../pages/Empresas";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Multas from "../pages/Multas";
-import NotFound from "../pages/NotFound";
 import TipoCepo from "../pages/TipoCepo";
 import Preferences from "../pages/Preferences";
+
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 export const privateRoutes = [
   {
